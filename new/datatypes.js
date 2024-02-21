e02747ce31dd69f5587ec59556f8fa00c22ad19d
surajpantha <153217234+surajpantha@users.noreply.github.com>
2024-02-21 23:07:09 +0545

    done

---
+++ new/datatypes.js
@@ -0,0 +1,18 @@
+"use strict"// treat all js code as never version
+// alert(3+3); //we are using node js not browser
+
+
+let name='suraj';//string
+let age="19";//number
+let isLoggedIn=false;//boolean
+
+
+//number=> 2raised to the power 53
+// bigint
+// string =>""
+// boolean =>true/false
+// null=>standalone value
+// undefined=>
+// symbol=>unique
+
+// object
--- new/first.js
+++ new/first.js
@@ -1,1 +1,7 @@
 console.log('hello world');
+
+// VARIABLE
+
+const accountId=144543;
+let accountEmail="random@gmail.com"
+var accountPassword="12345";
